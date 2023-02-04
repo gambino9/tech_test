cfeee8c0709c778e603e4314d1ddf647889ebbed
Lamia <lboukrou@student.42.fr>
2023-02-04 20:56:03 +0100

    dev: constructor for FileParser class, ameliorations, docstrings

--- src/file_parser.py
+++ src/file_parser.py
@@ -1,7 +1,4 @@
-import os
-import csv
 import pandas as pd
-import numpy as np
 
 
 # Parsing :
@@ -11,7 +8,7 @@ import numpy as np
 # - Incoherent datas                                YES
 # OTHERS
 # - Check for N/A values (dropping the columns)     YES
-# - Warning when float is found in volume column ?  NO
+# - Warning when float is found in volume column ?  YES
 
 # ANALYSIS :
 # - At the end of each file processing, display some metrics about data distribution
@@ -19,32 +16,36 @@ import numpy as np
 
 
 class FileParser:
-    def __init__(self):
-        pass
+    def __init__(self, df):
+        self.df = df
 
-    def find_missing_values(self, df):
-        """
-        Finds in the dataset all NaN (missing values) in the sentiments columns and the volume's column
-        :param df: Dataset
-        :return:
-        """
-        if any(df.isna()):
-            nan_total = df.isna().sum()  # A dataset containing nb of NaN for each column
-            sentiment_nan = sum(nan_total[['sentiment_positive', 'sentiment_negative', 'emotion_joy', 'emotion_fear']])
-            volume_nan = nan_total['volume']
-            print(f"There are {sentiment_nan} NaN values in sentiment datas, and {volume_nan} in volume")
-        # Remove missing values ?
-        df = df.dropna()
-        return df
+        self.nan_sentiment_values, self.nan_volume_values = self.find_missing_values()  # validity : no
+        self.date_time_continuity = self.check_date_time_continuity()  # validity : no
+        self.zeros_sentiment_values, self.zeros_volume_values = self.check_for_zero_values()  # validity : yes (warning)
+        self.float_volume_values = self.check_float_values_in_volume_column()  # validity : no
 
-    def check_date_time_continuity(self, df):  # Must tell how many missing lines are there
+    def find_missing_values(self):
         """
-        Iterates through dataset to check datetime discontinuity on a daily increasing basis
+        Finds in the dataset all NaN (missing values) in the sentiments columns and
+        the volume's column
+        :return: Tuple of number of NaN in the sentiments columns and number of Nan in volume column
+                 Tuple of None objects if no NaN values were found
+        """
+        if any(self.df.isna()):
+            nan_total = self.df.isna().sum()  # A dataset containing nb of NaN for each column
+            nan_sentiment_values = sum(nan_total[['sentiment_positive', 'sentiment_negative', 'emotion_joy', 'emotion_fear']])
+            nan_volume_values = nan_total['volume']
+            return nan_sentiment_values, nan_volume_values
+        return None, None
 
-        :param df: Time-series dataset
-        :return: Number of missing days in dataset
+    def check_date_time_continuity(self):  # Must tell how many missing lines are there
         """
-        d = pd.DatetimeIndex(df['Date'])
+        Iterates through DatetimeIndex obj from Date column of dataset to check
+        datetime discontinuity on a daily increasing basis
+
+        :return: Absolute value of missing days in dataset
+        """
+        d = pd.DatetimeIndex(self.df['Date'])
         i = 0
         discontinuities = 0
         while i < len(d) - 1:
@@ -56,19 +57,32 @@ class FileParser:
             i += 1
         return abs(int(discontinuities))
 
-    def check_outliers(self, df):
+    def check_for_zero_values(self):
         """
-        Checks for outliers values, values too far from the average by applying the inter quartile range method
-        :param df: Dataset
-        :return:
+        Counts numbers of zero values in each column of the dataframe
+        :return: Tuple containing number of zeros in sentiment columns and in volume column
+                 Tuple of None objects if no zero values found
+        """
+        zeros_df = self.df.isin([0]).sum(axis=0)
+        if sum(zeros_df):
+            zeros_sentiment_values = sum(zeros_df[['sentiment_positive', 'sentiment_negative', 'emotion_joy', 'emotion_fear']])
+            zeros_volume_values = zeros_df['volume']
+            return zeros_sentiment_values, zeros_volume_values
+        return None, None
+
+    def check_outliers(self, column_name):
+        """
+        For a given column, checks for outliers values (values too far
+        from the average) by applying the interquartile range method
+        :return: List containing outliers values
         """
         # inter quartile range method
-        q25, q75 = df['sentiment_negative'].quantile(0.25), df['sentiment_negative'].quantile(0.75)
+        q25, q75 = self.df[column_name].quantile(0.25), self.df[column_name].quantile(0.75)
         iqr = q75 - q25
-        cut_off = iqr * 1.5
+        cut_off = iqr * 0.5
         lower, upper = q25 - cut_off, q75 + cut_off
-        outliers = [x for x in df['sentiment_negative'] if x < lower or x > upper]
-        print(outliers)
+        outliers = [x for x in self.df[column_name] if x < lower or x > upper]
+        return outliers
 
         # data_mean, data_std = np.mean(df['sentiment_negative']), np.std(df['sentiment_negative'])
         # cut_off = data_std * 3  # 1.3
@@ -77,35 +91,20 @@ class FileParser:
         # outliers = [x for x in df['sentiment_negative'] if x < lower or x > upper]
         # print(outliers)
 
-    def check_for_zero_values(self, df):
-        """
-        Counts numbers of zero values in each column of the dataframe
-        :param df:
-        :return:
-        """
-        zeros_df = df.isin([0]).sum(axis=0)
-        if sum(zeros_df):
-            sentiment_zeros = sum(zeros_df[['sentiment_positive', 'sentiment_negative', 'emotion_joy', 'emotion_fear']])
-            volume_zeros = zeros_df['volume']
-            print(sentiment_zeros)
-            print(volume_zeros)
-        print(sum(zeros_df))
-
-    def check_float_values_in_volume_column(self, df):
+    def check_float_values_in_volume_column(self):
         """
         Counts number of zero values in the dataset
-        :param df:
-        :return:
+        :return: Number of float values in volume column, else None
         """
-        volume_series = df['volume']
+        volume_series = self.df['volume']
         if float_values := sum(not float(x).is_integer() for x in volume_series):
-            print(f"Warning : There are {float_values} in the volume column")
+            return float_values
+        return None
 
-    def print_csv_file_analysis(self, df):
-        """
-        Prints
-        :param df:
-        :return:
-        """
+    def is_dataset_valid(self):
+        pass
+
+    def print_csv_file_analysis(self):
+        pass
 
 
