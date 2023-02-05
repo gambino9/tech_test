a70f25c667948843fc9b34b5b1b5806230ce4f9d
Lamia <lboukrou@student.42.fr>
2023-02-05 18:42:42 +0100

    dev: main with cli is done

--- src/main.py
+++ src/main.py
@@ -1,21 +1,22 @@
-import os
-import pandas as pd
 from file_loader import FileLoader
 from file_parser import FileParser
+import click
+
+
+@click.command()
+@click.argument('file', type=click.Path(exists=True))
+def main(file):
+    """
+    The script takes a file path to analyze a dataset and print results
+
+    FILE is the argument path
+    """
+    file_loader = FileLoader()
+    dataframe = file_loader.load_file(file)
+
+    file_parser = FileParser(dataframe)
+    file_parser.check_dataset_validity()
 
 
 if __name__ == "__main__":
-    dirname = os.path.dirname(__file__)
-    data_file = os.path.join(dirname, '../resources/test_file (copie).csv')
-    # csv_file = os.path.join(dirname, '../resources/test_file.csv')
-    file_loader = FileLoader()
-    dataframe = file_loader.load_file(data_file)
-
-    file_parser = FileParser()
-
-    # file_parser.find_missing_values(dataframe)
-    file_parser.check_float_values_in_volume_column(dataframe)
-    # check_values_range(dataframe)
-    # print(check_date_time_continuity(dataframe))
-    # check_for_zero_values(dataframe)
-    # check_outliers(dataframe)
+    main()
